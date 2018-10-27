4c6f6e13cb33cdf9b720f13c7b22abcec01308ad
Vlad Denisov <vlad@vdenisov.net>
2018-10-26 21:31:26 -0400

    Finished notes application features

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('Starting App.');
+console.log('Starting App.\n');
 /**
  * Logic for require:
  *   1. Look for Core package.
@@ -6,19 +6,68 @@ console.log('Starting App.');
  *   3. Look for relative path.
  */
 const fs = require('fs');
-const os = require('os');
 const _ = require("lodash");
+const yargs = require('yargs');
 const notes = require('./notes');
 
-const username = os.userInfo().username;
+const titleOptions = {
+    describe: "Title of note",
+    demand: true,
+    alias: "t"
+};
 
-console.log(_.isString(true));
-console.log(_.isString('Test'));
+const bodyOptions = {
+    describe: "Body of note",
+    demand: true,
+    alias: "t"
+};
 
-console.log(_.uniq([2,1,2]));
+const argv = yargs
+  .command("add", "Add a new note", {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command("read", "Read a note", {
+    title: titleOptions
+  })
+  .command("remove", "Remove a note", {
+    title: titleOptions
+  })
+  .command("list", "List all notes")
+  .help().argv;
+  
+const command = argv._.pop();
 
-fs.appendFile('greetings.txt', `Hello ${username}! You are ${notes.age}.\n`, (err) => {
-    if(err) {
-        console.log(`Unable to write to file: ${err}`);
+if(command === "add") {
+    let note = notes.addNote(argv.title, argv.body);
+
+    if (typeof note !== "undefined") {
+        console.log(`Added note ${note.title}: ${note.body}`);
+    } else {
+        console.log(`Note with title ${argv.title} already exists`);
     }
-});
+} else if(command === "list") {
+    let allNotes = notes.getAll();
+    console.log(`${allNotes.length} notes:`);
+
+    for (note of allNotes) {
+      console.log(`  ${note.title}`);
+    }
+} else if (command === "read") {
+    let note = notes.readNote(argv.title);
+
+    if (typeof note !== "undefined") {
+      console.log(`${note.title}:\n${note.body}\n\n`);
+    } else {
+      console.log(`Note ${argv.title} does not exist`);
+    }
+
+ } else if (command === "remove") {
+    if (notes.removeNote(argv.title)) {
+      console.log(`Removed Note ${argv.title}`);
+    } else {
+      console.log(`Note ${argv.title} does not exist`);
+    }
+ } else {
+    console.log("Command not recognized");
+}
--- notes.js
+++ notes.js
@@ -1,11 +1,47 @@
-console.log('Starting notes.js');
+const fs = require('fs');
+const NOTE_FILENAME = 'notes-data.json';
 
-module.exports.addNote = () => {
-    // Arrow function does not bind the keyword 'this'.
-    console.log('addNote');
-    return 'New note';
+if(!fs.existsSync('notes-data.json')) {
+    fs.writeFileSync(NOTE_FILENAME, "[]");
+    console.log(`File ${NOTE_FILENAME} not found. Created file.`);
+}
+
+let notes = require(`./${NOTE_FILENAME}`);
+
+let noteExists = (title) => {
+    return typeof notes.find((element) => { return element.title === title; }) !== 'undefined';
 };
 
-module.exports.add = (a, b) => {
-    return a + b;
+let addNote = (title, body) => {
+    if(!noteExists(title)) {
+        const note = { title: title, body: body };
+        notes.push(note);
+        fs.writeFileSync(NOTE_FILENAME, JSON.stringify(notes));
+        return note;
+    }
+};
+
+let getAll = () => {
+    return notes;
+};
+
+let readNote = (title) => {
+    return notes.find((element) => { return element.title === title });
+};
+
+let removeNote = (title) => {
+    if(noteExists(title)){
+        notes = notes.filter((element) => { return element.title !== title; });
+        fs.writeFileSync(NOTE_FILENAME, JSON.stringify(notes));
+        return true;
+    }
+
+    return false;
+};
+
+module.exports = {
+    addNote: addNote,
+    getAll: getAll,
+    removeNote: removeNote,
+    readNote: readNote
 };
